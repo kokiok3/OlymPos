63f0f6b353ba6855945f1f1942331d1ec055820e
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-08-07 23:03:51 +0900

    Feat: router/index.ts : 계정만들기 경로 추가

--- frontend/olympos-frontend/src/router/index.ts
+++ frontend/olympos-frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/LoginView.vue'
+import SignUp from '@/views/SignUpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,10 +11,10 @@ const router = createRouter({
       component: Login
     },
     {
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-    }
+      path: '/sign-up',
+      name: 'sign-up',
+      component: SignUp
+    },
   ]
 })
 
