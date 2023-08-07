5192211d63bc4a8c74d9e9f60534afb42757187b
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-08-07 23:48:47 +0900

    Refactor: router/index.ts : 파일명 및 경로 수정

--- frontend/olympos-frontend/src/router/index.ts
+++ frontend/olympos-frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/LoginView.vue'
-import SignUp from '@/views/SignUpView.vue'
+import SignUpUserNameView from '@/views/SignUpUserNameView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,9 +11,10 @@ const router = createRouter({
       component: Login
     },
     {
-      path: '/sign-up',
-      name: 'sign-up',
-      component: SignUp
+      path: '/sign-up/user-name',
+      name: 'sign-up/user-name',
+      component: SignUpUserNameView
+    },
     },
   ]
 })
