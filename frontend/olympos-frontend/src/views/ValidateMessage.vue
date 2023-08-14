0192e7b03d2e9ac98c67a1ecbbec23685887800b
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-08-14 23:46:25 +0900

    Refactor: LoginView.vue : 코드 모듈화

---
+++ frontend/olympos-frontend/src/views/ValidateMessage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const validateMsg = defineProps({
+    errorMsg: String
+});
+</script>
+
+<template>
+    <div class="validate-error">
+        <i class="fa-solid fa-circle-exclamation"></i>
+        <span>{{ validateMsg.errorMsg }}</span>
+    </div>
+</template>
+
+<style scoped>
+</style>
