4d2f782fe4d2a06601747a6ed01e72aa815c1951
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-09-05 00:17:02 +0900

    Feat: ValidateSignUp.ts : 유효성 추가

--- frontend/olympos-frontend/src/validations/ValidateSignUp.ts
+++ frontend/olympos-frontend/src/validations/ValidateSignUp.ts
@@ -1,6 +1,9 @@
+const required = '필수 정보 입니다.';
 const ValidateSignUp = {
     firstName: '문자만 입력해주세요.',
-    lastName: '필수 정보 입니다.'
+    lastName: required,
+    phone: required,
+    email: required
 }
 
 export default ValidateSignUp;
