822ceed42e6f4b5438084846d8a70c68f4a5e0e6
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-10-19 23:09:42 +0900

    Feat: ButtonRectangle.vue 생성

---
+++ frontend/olympos-frontend/src/components/buttons/ButtonRectangle.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="button_rectangle"><slot></slot></button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.button_rectangle {
+    cursor: pointer;
+    padding: 9px 22px;
+    font-size: 13px;
+    font-weight: bold;
+    border-radius: 5px;
+    border: 0;
+    background-color: var(--main-mute-1);
+    color: var(--main-black);
+}
+</style>
