46bb64ff09a1bc036a3600188575cec5b13b7819
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-10-19 23:11:11 +0900

    Feat: StoreManagement.vue 생성

---
+++ frontend/olympos-frontend/src/views/stores/StoresManagement.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="store_content">
+        <ImageSlider />
+        <div class="store_wrapper">
+            <ContentView>
+                <template #title>
+                    매장 관리
+                </template>
+
+                <template #buttonArea>
+                    <ButtonRectangle>매장 추가</ButtonRectangle>
+                </template>
+
+                <template #table>
+                    ContentTable
+                </template>
+            </ContentView>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import ImageSlider from '@/views/stores/ImageSlider.vue';
+import ContentView from '@/components/contents/ContentView.vue';
+import ButtonRectangle from '@/components/buttons/ButtonRectangle.vue';
+
+</script>
+
+<style scoped>
+.image_slider {
+    margin: -50px;
+    margin-bottom: 0;
+}
+.store_wrapper {
+    margin-top: 50px;
+}
+</style>
