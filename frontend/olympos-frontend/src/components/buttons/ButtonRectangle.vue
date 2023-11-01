fffcd784f323a5fa9bfb4b9e5aabb2a0ed9b4608
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-11-01 23:25:53 +0900

    Design: ButtonInTable, ButtonRectangle 컴포넌트

--- frontend/olympos-frontend/src/components/buttons/ButtonRectangle.vue
+++ frontend/olympos-frontend/src/components/buttons/ButtonRectangle.vue
@@ -16,5 +16,9 @@
     border: 0;
     background-color: var(--main-mute-1);
     color: var(--main-black);
+    transition: 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+.button_rectangle:hover {
+    background-color: var(--main-soft-1);
 }
 </style>
