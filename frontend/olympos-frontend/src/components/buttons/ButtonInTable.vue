604ada4375cb3fbf1523462cda010571eff85e09
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-11-01 23:00:05 +0900

    Feat: ButtonInTable.vue : TableView에 들어갈 버튼 컴포넌트

---
+++ frontend/olympos-frontend/src/components/buttons/ButtonInTable.vue
@@ -0,0 +1,19 @@
+<template>
+    <button class="button_in_table"><slot></slot></button>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.button_in_table {
+    cursor: pointer;
+    padding: 9px 22px;
+    font-size: 13px;
+    font-weight: bold;
+    border-radius: 5px;
+    border: 0;
+    background-color: var(--main-mute-1);
+    color: var(--main-black);
+}
+</style>
