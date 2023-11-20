bd976f2f87a269e36b24e22ddd7fb66dcd1ff5e0
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2023-11-21 00:02:27 +0900

    Feat: 매장 추가 페이지 생성, 매장 추가 페이지를 위한 컴포넌트 생성

---
+++ frontend/olympos-frontend/src/components/management/managementInput.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="input_title">
+            {{ props.title }}
+        </div>
+        <input type="text" :placeholder="dynamicPlaceholder" @focus="focus" @blur="blur">
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+    title: String,
+    placeholder: String,
+});
+
+const dynamicPlaceholder = ref(props.placeholder);
+const focus = ()=>{
+    dynamicPlaceholder.value = '';
+}
+const blur = ()=>{
+    dynamicPlaceholder.value = props.placeholder;
+}
+</script>
+
+<style scoped>
+.input_title {
+    padding: 10px 0;
+    font-weight: bold;
+    font-size: 13px;
+    border-bottom: 1px solid var(--main-gray-3);
+}
+input {
+    width: -webkit-fill-available;
+    border: none;
+    outline: none;
+}
+input::placeholder {
+    color: var(--main-gray-2);
+}
+</style>
---
+++ frontend/olympos-frontend/src/views/stores/CreateStore.vue
@@ -0,0 +1,30 @@
+<template>
+    <ManagementView>
+        <template #title>
+            매장 추가
+        </template>
+
+        <template #form>
+            <ManagementInput :title="'매장 명'" :placeholder="'운영하고 계신 매장 이름을 입력해 주세요.'"></ManagementInput>
+            <ManagementInput :title="'전화 번호'" :placeholder="'해당 매장의 전화번호를 입력해 주세요.'"></ManagementInput>
+            <ManagementInput :title="'주소'" :placeholder="'해당 매당의 주소를 입럭해 주세요.'"></ManagementInput>
+            <ManagementInput :title="'사장님 성함'" :placeholder="'사장님 성함을 입력해 주세요.'"></ManagementInput>
+            <ManagementInput :title="'테이블 개수'" :placeholder="'매장 내의 테이블 개수를 입력해 주세요.'"></ManagementInput>
+        </template>
+
+        <template #btnArea>
+            <ManagementButton :btnColor="'white'">취소</ManagementButton>
+            <ManagementButton :btnColor="'blue'">매장 등록</ManagementButton>
+        </template>
+    </ManagementView>
+</template>
+
+<script setup lang="ts">
+import ManagementView from '@/components/management/ManagementView.vue';
+import ManagementInput from '@/components/management/ManagementInput.vue';
+import ManagementButton from '@/components/management/ManagementButton.vue';
+
+</script>
+
+<style scoped>
+</style>
