e0cbcf1bedebadafce6920cda82dd726cba57024
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2024-03-04 23:43:41 +0900

    Feat: StoreApi.ts : error시 얼럿창 띄우기

--- frontend/olympos-frontend/src/apis/StoreApi.ts
+++ frontend/olympos-frontend/src/apis/StoreApi.ts
@@ -1,6 +1,8 @@
 import DefaultAxios from '@/apis/DefaultApi';
 import { type createStoreBody } from '@/types/StoreTypes';
 import { ACCESS_TOKEN } from '@/functions/AccessToken';
+import { push } from 'notivue';
+import { API_CODE } from '@/constants/ApiCodeConstant';
 
 const StoreApi = {
     createStore(params: createStoreBody){
@@ -11,15 +13,17 @@ const StoreApi = {
         })
         .then(res=>{
             if(res.data.code === 100 && res.data.result === "Success"){
-                return;
+                return true;
             }
             else {
                 throw new Error(res.data.code);
             }
         })
         .catch(error=>{
-            // todo: console.log대신 오류 메세지 창 생성하기
-            console.log('err: ', error.message);
+            push.error({
+                title: '에러',
+                message: API_CODE[error.message],
+            });
         })
     }
 }
