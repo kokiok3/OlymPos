aedf99e0fcb70d635e8a35b51cf3f7dedeb478f6
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2024-03-04 23:42:53 +0900

    Feat: AcessToken.ts 생성 및 StoreApi.ts에 적용

--- frontend/olympos-frontend/src/apis/StoreApi.ts
+++ frontend/olympos-frontend/src/apis/StoreApi.ts
@@ -1,9 +1,14 @@
 import DefaultAxios from '@/apis/DefaultApi';
 import { type createStoreBody } from '@/types/StoreTypes';
+import { ACCESS_TOKEN } from '@/functions/AccessToken';
 
 const StoreApi = {
     createStore(params: createStoreBody){
-        return DefaultAxios.post('/add-store', params)
+        return DefaultAxios.post('/add-store', params, {
+            headers: {
+                Authorization: ACCESS_TOKEN()
+            }
+        })
         .then(res=>{
             if(res.data.code === 100 && res.data.result === "Success"){
                 return;
