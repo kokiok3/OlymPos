805eac140cfcd1abea22999cd39d687f0a3318d6
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2024-05-27 23:19:37 +0900

    Refactor: ContentView.vue : class명 변경

--- frontend/olympos-frontend/src/components/contents/ContentView.vue
+++ frontend/olympos-frontend/src/components/contents/ContentView.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <ContentTitle class="content_title"><slot name="title"></slot></ContentTitle>
-        <ContentControlArea class="content_btn_area"><slot name="controlArea"></slot></ContentControlArea>
+        <ContentControlArea class="content_control_area"><slot name="controlArea"></slot></ContentControlArea>
         <ContentTable><slot name="table"></slot></ContentTable>
     </div>
 </template>
@@ -17,7 +17,7 @@ import ContentTable from '@/components/contents/ContentTable.vue';
 .content_title {
     font-size: 21px;
 }
-.content_btn_area {
+.content_control_area {
     margin: 30px 0;
     display: flex;
     justify-content: flex-end;
