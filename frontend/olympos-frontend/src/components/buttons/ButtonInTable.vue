1a30fe2e64b2bfdcbe78d4fcfdf4676a09cacbde
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2025-01-06 22:10:16 +0900

    Design: 버튼 디자인 변경

--- frontend/olympos-frontend/src/components/buttons/ButtonInTable.vue
+++ frontend/olympos-frontend/src/components/buttons/ButtonInTable.vue
@@ -9,13 +9,12 @@
 .button_in_table {
     cursor: pointer;
     padding: 4px 10px;
-    font-size: 13px;
-    font-weight: bold;
-    border-radius: 5px;
+    font-size: 12px;
+    border-radius: 2px;
     border: 0;
     background-color: var(--main-mute-1);
     color: var(--main-black);
-    box-shadow: rgba(15, 15, 15, 0.1) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 2px 4px;
+    box-shadow: rgba(15, 15, 15, 0.1) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 2px 0px;
     transition: 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
 }
 .button_in_table:hover {
